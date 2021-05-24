d5d3ed1596a6661436db8ac2422e7833546ed49e
lahmyycfire <546090788@qq.com>
2021-05-24 18:13:05 +0800

    orm

--- test.py
+++ test.py
@@ -1,1 +1,23 @@
-print('test')
+import asyncio, os, json, time
+from datetime import datetime
+import logging; logging.basicConfig(level=logging.INFO)
+from aiohttp import web
+
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
+
+
+async def init(loop):
+    app = web.Application(loop=loop, )
+    app.router.add_route('GET', '/', index)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, '127.0.0.1', 8000)
+    logging.info('Server started at http://127.0.0.1:8000...')
+    await site.start()
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
